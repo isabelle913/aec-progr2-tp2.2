dbc5994630fdda05c8617aabe30cfd27bb291482
isabelle913 <atelierisabelle@videotron.ca>
2023-08-29 23:07:02 -0400

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
+import AllCategories from "./meals/components/AllCategories/AllCategories";
+import MealsByCategory from "./meals/components/MealsByCategory/MealsByCategory";
+import MealDetails from "./meals/components/MealDetails/MealDetails";
+
 function App() {
   const queryClient = new QueryClient();
 
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        Bonjour
         <Routes>
-          <Route></Route>
+          <Route path="/" element={<AllCategories />} />
+          <Route path="/category/:category" element={<MealsByCategory />} />
+          <Route path="/details/:idMeal" element={<MealDetails />} />
         </Routes>
       </QueryClientProvider>
     </>
