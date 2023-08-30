ed9f81131583ab49682d79c1e07a3be9bdfc0dea
isabelle913 <atelierisabelle@videotron.ca>
2023-08-29 23:05:57 -0400

    Appel à l'api

---
+++ src/meals/services/MealsServices.js
@@ -0,0 +1,61 @@
+// Docs https://www.themealdb.com/api.php
+
+// List all meal categories
+//www.themealdb.com/api/json/v1/1/categories.php
+
+// Filter by Category
+// www.themealdb.com/api/json/v1/1/filter.php?c=Seafood
+
+// Lookup full meal details by id
+// www.themealdb.com/api/json/v1/1/lookup.php?i=52772
+
+// base url www.themealdb.com/api/json/v1/1/
+
+import { config } from "../../config";
+
+class MealsService {
+  baseUrl = config.baseUrl;
+  endpointAll = "categories.php";
+  endpointOneCategory = "filter.php?c=";
+  endpointDetail = "lookup.php?i=";
+
+  async getAllCategories() {
+    const response = await fetch(`${this.baseUrl}${this.endpointAll}`);
+
+    if (!response.ok)
+      throw new Error(`Error:getAllCategories ${response.status}`);
+
+    const categories = await response.json();
+    //console.log("getAllCategories -> ", categories);
+
+    return categories;
+  }
+
+  async getMealsByCategory(category) {
+    const response = await fetch(
+      `${this.baseUrl}${this.endpointOneCategory}${category}`
+    );
+
+    if (!response.ok)
+      throw new Error(`Error:getMealsByCategory ${response.status}`);
+
+    const categoryMeals = await response.json();
+    //console.log("getMealsByCategory -> ", categoryMeals);
+
+    return categoryMeals;
+  }
+
+  async getMealDetailById(id) {
+    const response = await fetch(`${this.baseUrl}${this.endpointDetail}${id}`);
+
+    if (!response.ok)
+      throw new Error(`Error:getMealDetailById ${response.status}`);
+
+    const mealDetail = await response.json();
+    //console.log("getMealDetailById -> ", mealDetail);
+
+    return mealDetail;
+  }
+}
+
+export default MealsService;
